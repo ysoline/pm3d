54e0c4eaba935e685d3197cf3715a07dff5a1a89
ysoh <juliepilarski@yahoo.fr>
2019-10-01 18:55:11 +0200

    Mise à jour du Manager et Template du site

--- Models/Manager.php
+++ Models/Manager.php
@@ -5,7 +5,7 @@ class Manager
     protected function dbConnect()
     {
         //INSTANCIE LA CONNEXION
-        $_bdd = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
+        $_bdd = new PDO('mysql:host=localhost;dbname=pm;charset=utf8', 'root', '');
         $_bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 
         return $_bdd;
